4b7cdc67037d660376500f9ca1c1b16ae4893c32
Richard Maxwell <richard@rsmaxwell.co.uk>
2020-03-02 12:32:01 +0000

    add diary key to query string

--- cmd/page/page.go
+++ cmd/page/page.go
@@ -34,7 +34,7 @@ func main() {
 
 	requestURI, exists := os.LookupEnv("REQUEST_URI")
 	if !exists {
-		fmt.Fprintf(os.Stderr, "environment variable 'REQUEST_URI' not found\n")
+		fmt.Fprintf(os.Stderr, "environment variable 'REQUEST_URI' not foundIndex\n")
 	}
 
 	u, err := url.Parse(requestURI)
@@ -68,6 +68,22 @@ func main() {
 		}
 	}
 
+	diaries := q["diary"]
+	if len(diaries) < 1 {
+		fmt.Fprintf(os.Stderr, "Missing 'diary' key in requestURI query\n")
+		fmt.Fprintf(os.Stderr, "    requestURI: %s\n", requestURI)
+		fmt.Fprintf(os.Stderr, "    q: %+v\n", q)
+		os.Exit(1)
+	} else if len(diaries) > 1 {
+		fmt.Fprintf(os.Stderr, "too many 'diary' keys in requestURI\n")
+		fmt.Fprintf(os.Stderr, "    requestURI: %s\n", requestURI)
+		for i, d := range diaries {
+			fmt.Fprintf(os.Stderr, "    diary[%d]: %s\n", i, d)
+		}
+	}
+
+	diary := diaries[0]
+
 	files := q["image"]
 	if len(files) < 1 {
 		fmt.Fprintf(os.Stderr, "Missing 'image' key in requestURI query\n")
@@ -84,12 +100,13 @@ func main() {
 
 	filename := files[0]
 
-	imagefile := filepath.Join(config.Prefix, filename)
+	imagefile := filepath.Join(config.Prefix, diary, filename)
 	_, err = os.Stat(imagefile)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not stat image file\n")
 		fmt.Fprintf(os.Stderr, "    image file: %s\n", imagefile)
 		fmt.Fprintf(os.Stderr, "    config.Prefix: %s\n", config.Prefix)
+		fmt.Fprintf(os.Stderr, "    diary: %s\n", diary)
 		fmt.Fprintf(os.Stderr, "    filename: %s\n", filename)
 	}
 
@@ -115,62 +132,52 @@ func main() {
 		return filelist[i].Name() < filelist[j].Name()
 	})
 
-	found := -1
+	foundIndex := -1
 	for i, f := range filelist {
 		if filepath.Base(filename) == f.Name() {
-			found = i
+			foundIndex = i
 		}
 	}
 
-	if found < 0 {
+	if foundIndex < 0 {
 		fmt.Fprintf(os.Stderr, "file not found: %s\n", filename)
 	}
 
-	previousButton := ""
-	if found > 0 {
-		prev := filelist[found-1]
-		previousFile := filepath.Join(config.Prefix, prev.Name())
-		previousButton = " <div class=\"center-left\">" +
-			"<a href=\"" + previousFile + "\">" +
+	cgi := filepath.Join(config.Prefix, "page")
+	_, err = os.Stat(cgi)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not stat cgi file: %s\n", cgi)
+	}
+
+	previousHTML := ""
+	if foundIndex > 0 {
+		prev := filelist[foundIndex-1]
+		previousURL := cgi + "?diary=" + diary + "&image=" + prev.Name()
+		previousHTML = " <div class=\"center-left\">" +
+			"<a href=\"" + previousURL + "\">" +
 			"<img src=\"images/previous.png\" >" +
 			"</a>" +
 			"</div> \n"
-
-		_, err = os.Stat(previousFile)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Could not stat previous file\n")
-			fmt.Fprintf(os.Stderr, "    previousFile: %s\n", previousFile)
-			fmt.Fprintf(os.Stderr, "    config.Prefix: %s\n", config.Prefix)
-			fmt.Fprintf(os.Stderr, "    prev.Name(): %s\n", prev.Name())
-		}
 	}
 
-	nextButton := ""
-	if found < len(filelist) {
-		next := filelist[found+1]
-		nextFile := filepath.Join(config.Prefix, next.Name())
-		nextButton = " <div class=\"center-right\">" +
-			"<a href=\"" + nextFile + "\">" +
+	nextHTML := ""
+	if foundIndex < len(filelist) {
+		next := filelist[foundIndex+1]
+		nextURL := cgi + "?diary=" + diary + "&image=" + next.Name()
+		nextHTML = " <div class=\"center-right\">" +
+			"<a href=\"" + nextURL + "\">" +
 			"<img src=\"images/next.png\" >" +
 			"</a>" +
 			"</div> \n"
-
-		_, err = os.Stat(nextFile)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Could not stat previous file\n")
-			fmt.Fprintf(os.Stderr, "    nextFile: %s\n", nextFile)
-			fmt.Fprintf(os.Stderr, "    config.Prefix: %s\n", config.Prefix)
-			fmt.Fprintf(os.Stderr, "    next.Name(): %s\n", next.Name())
-		}
 	}
 
-	zoomButton := ""
+	zoomHTML := ""
 	image := ""
 	if zoom == "scale" {
-		zoomButton = " <div class=\"top-center\"><img src=\"images/minus.png\"></div> \n"
+		zoomHTML = " <div class=\"top-center\"><img src=\"images/minus.png\"></div> \n"
 		image = " <img src=\"" + imagefile + "\" class=\"center-fit\" > \n"
 	} else {
-		zoomButton = " <div class=\"top-center\"><img src=\"images/plus.png\"></div> \n"
+		zoomHTML = " <div class=\"top-center\"><img src=\"images/plus.png\"></div> \n"
 		image = " <img src=\"" + imagefile + "\" > \n"
 	}
 
@@ -184,9 +191,9 @@ func main() {
 		"<body> \n" +
 		"<div class=\"imgbox\"> \n" +
 		image +
-		previousButton +
-		zoomButton +
-		nextButton +
+		previousHTML +
+		zoomHTML +
+		nextHTML +
 		"</div> \n" +
 		"</body> \n" +
 		"</html> \n"
