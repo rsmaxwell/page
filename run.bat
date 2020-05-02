bc2c82af1ef7c3363ac6f563a0e0b8afeff3b752
Richard Maxwell <richard@rsmaxwell.co.uk>
2020-05-02 17:49:55 +0100

    simplify promotion process

---
+++ run.bat
@@ -0,0 +1,5 @@
+
+set PAGE_CONFIGFILE=C:/temp/12/page.json
+set REQUEST_URI=https://server.rsmaxwell.co.uk/cgi-bin/page?diary=diary-1828-and-1829-and-jan-1830^&image=img2834.jpg
+
+build\page-amd64-windows-0.0.SNAPSHOT.exe 1> C:/temp/12/diaries/page-generated.html
